7ea52131d8bda9e59bd9104a5c065ba2b7940350
xyskin <xyskin@163.com>
2018-01-27 11:59:09 +0000

    push it

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    private Vector3 screenPos;
+	// Use this for initialization
+	void Start () {
+        screenPos = new Vector3();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.W))
+        {
+            screenPos = Camera.main.WorldToScreenPoint(this.transform.position);
+            screenPos = new Vector3(screenPos.x, screenPos.y + 0.1f, screenPos.z);
+            this.transform.position = Camera.main.ScreenToWorldPoint(new Vector3(screenPos.x,screenPos.y,1.0f));
+        }
+	}
+}
