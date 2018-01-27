930625df43682c473791e30fa1484c24eecdb84e
xyskin <xyskin@163.com>
2018-01-27 13:06:07 +0000

    fixed movement bugs

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,50 +2,112 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : MonoBehaviour
+{
     float xspeed, zspeed;
-	// Use this for initialization
-	void Start () {
+    bool xp1, xp2, zp1, zp2;
+    // Use this for initialization
+    void Start()
+    {
         xspeed = 0;
         zspeed = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-        this.transform.position = new Vector3(this.transform.position.x + xspeed * Time.deltaTime,
-            this.transform.position.y, this.transform.position.z + zspeed * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.D))
+        zp1 = false;
+        xp1 = false;
+        zp2 = false;
+        xp2 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.position = new Vector3(this.transform.position.x + xspeed * Time.deltaTime,
+        //    this.transform.position.y, this.transform.position.z + zspeed * Time.deltaTime);
+        this.GetComponent<Rigidbody>().velocity = new Vector3(xspeed, 0, zspeed);
+        if (this.name == "Player1")
         {
-            xspeed = 1f;
+            if (Input.GetKey(KeyCode.D) && !xp2)
+            {
+                xspeed = 3f;
+                xp1 = true;
+            }
+            if (Input.GetKeyUp(KeyCode.D))
+            {
+                xspeed = 0f;
+                xp1 = false;
+            }
+            if (Input.GetKey(KeyCode.A) && !xp1)
+            {
+                xspeed = -3f;
+                xp2 = true;
+            }
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                xspeed = 0;
+                xp2 = false;
+            }
+            if (Input.GetKey(KeyCode.W) && !zp2)
+            {
+                zspeed = 3f;
+                zp1 = true;
+            }
+            if (Input.GetKeyUp(KeyCode.W))
+            {
+                zspeed = 0f;
+                zp1 = false;
+            }
+            if (Input.GetKeyUp(KeyCode.S))
+            {
+                zspeed = 0f;
+                zp2 = false;
+            }
+            if (Input.GetKey(KeyCode.S) && !zp1)
+            {
+                zspeed = -3f;
+                zp2 = true;
+            }
         }
-        if (Input.GetKeyUp(KeyCode.D))
+        if (this.name == "Player2")
         {
-            xspeed = 0f;
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            xspeed = -1f;
-        }
-        if (Input.GetKeyUp(KeyCode.A))
-        {
-            xspeed = 0;
-        }
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            zspeed = 1f;
-        }
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            zspeed = 0f;
-        }
-        if (Input.GetKeyUp(KeyCode.S))
-        {
-            zspeed = 0f;
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            zspeed = -1f;
+            if (Input.GetKey(KeyCode.RightArrow)&&!xp1)
+            {
+                xspeed = 3f;
+                xp2 = true;
+            }
+            if (Input.GetKeyUp(KeyCode.RightArrow))
+            {
+                xspeed = 0f;
+                xp2 = false;
+            }
+            if (Input.GetKey(KeyCode.LeftArrow) && !xp2)
+            {
+                xspeed = -3f;
+                xp1 = true;
+            }
+            if (Input.GetKeyUp(KeyCode.LeftArrow))
+            {
+                xspeed = 0;
+                xp1 = false;
+            }
+            if (Input.GetKey(KeyCode.UpArrow) && !zp1)
+            {
+                zspeed = 3f;
+                zp2 = true;
+            }
+            if (Input.GetKeyUp(KeyCode.UpArrow))
+            {
+                zspeed = 0f;
+                zp2 = false;
+            }
+            if (Input.GetKeyUp(KeyCode.DownArrow))
+            {
+                zspeed = 0f;
+                zp1 = false;
+            }
+            if (Input.GetKey(KeyCode.DownArrow) && !zp2)
+            {
+                zspeed = -3f;
+                zp1 = true;
+            }
         }
     }
 }
