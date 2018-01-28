280a876f498588ab6a2cea2cf25da3e6761ae7f8
xyskin <xyskin@163.com>
2018-01-28 13:18:56 +0000

    sss

---
+++ Assets/Scripts/BuildingSlider.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingSlider : MonoBehaviour {
+
+    public Slider slider;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (this.GetComponent<BuildingFollowers>().build)
+        {
+            case Static.BuildBl.player1:
+                slider.GetComponentsInChildren<Image>()[0].color = new Color(0.7f, 0.2f, 0.2f);
+                slider.GetComponentsInChildren<Image>()[1].color = new Color(0.2f, 0.7f, 0.2f);
+                slider.GetComponentsInChildren<Image>()[2].color = new Color(0.7f, 0.2f, 0.2f);
+
+                slider.GetComponent<Slider>().value = (float)this.GetComponent<BuildingFollowers>().greenfollower / (float)this.GetComponent<BuildingFollowers>().followerQuan;
+                break;
+            case Static.BuildBl.player2:
+                slider.GetComponentsInChildren<Image>()[0].color = new Color(0.2f, 0.7f, 0.2f);
+                slider.GetComponentsInChildren<Image>()[1].color = new Color(0.7f, 0.2f, 0.2f);
+                slider.GetComponentsInChildren<Image>()[2].color = new Color(0.2f, 0.7f, 0.2f);
+                slider.GetComponent<Slider>().value = (float)this.GetComponent<BuildingFollowers>().redfollower / (float)this.GetComponent<BuildingFollowers>().followerQuan;
+
+                break;
+           default:
+                slider.GetComponentsInChildren<Image>()[0].color = new Color(1.0f, 1.0f, 1.0f);
+
+
+                if (this.GetComponent<BuildingFollowers>().redfollower > this.GetComponent<BuildingFollowers>().greenfollower)
+                {
+
+                    slider.GetComponent<Slider>().value = (float)this.GetComponent<BuildingFollowers>().redfollower / (float)this.GetComponent<BuildingFollowers>().followerQuan;
+                    slider.GetComponentsInChildren<Image>()[2].color = new Color(0.7f, 0.2f, 0.2f);
+                }
+                if (this.GetComponent<BuildingFollowers>().redfollower < this.GetComponent<BuildingFollowers>().greenfollower)
+                {
+                    slider.GetComponent<Slider>().value = (float)this.GetComponent<BuildingFollowers>().greenfollower / (float)this.GetComponent<BuildingFollowers>().followerQuan;
+
+                    slider.GetComponentsInChildren<Image>()[2].color = new Color(0.2f, 0.7f, 0.2f);
+                }
+                break;
+        }
+        //slider.GetComponentsInChildren<Image>()[0].color = new Color();
+        //slider.GetComponentsInChildren<Image>()[1].color = new Color();
+        //slider.GetComponentsInChildren<Image>()[2].color = new Color();
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private GameObject p1, p2;
+    public
+	// Use this for initialization
+	void Start () {
+        p1 = GameObject.FindGameObjectWithTag("Player1");
+        p2 = GameObject.FindGameObjectWithTag("Player2");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (p1.GetComponent<PlayerFollowers>().HP <= 0)
+        {
+
+        }
+        if (p2.GetComponent<PlayerFollowers>().HP <= 0)
+        {
+
+        }
+    }
+}
